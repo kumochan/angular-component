ef72c2515d3501c21dca892b2a276d29c414ea72
Thao Trinh Minh <thaotrinhminh@gmail.com>
2020-12-18 09:44:04 +0700

    udpate articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -48,6 +48,7 @@ export class AppComponent {
   public myListArticles: Article[] = [
     { title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await', url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40' },
     { title: 'Game of Life', url: 'https://thefullsnack.com/posts/game-of-life.html' },
+    { title: 'Game', url: 'Diablo' },
   ];
 
   addArticle()  {
